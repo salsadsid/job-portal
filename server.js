7980953434ead3e999d94ce259a9ac64751de300
Salman Sadik Siddiquee <salman.dnj@gmail.com>
2022-10-14 01:05:11 +0600

    bug-fixed

--- server.js
+++ server.js
@@ -5,8 +5,24 @@ const colors = require("colors");
 const app = require("./app");
 
 // database connection
-mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
-    console.log("Database in Connected".red.bold)
+// mongoose.connect(process.env.DATABASE_LOCAL).then(() => {
+//     console.log("Database in Connected".red.bold)
+// })
+mongoose.connect(process.env.DATABASE_LOCAL, {
+
+    useNewUrlParser: "true",
+    useUnifiedTopology: "true"
+
+})
+mongoose.connection.on("error", err => {
+
+    console.log("err", err)
+
+})
+mongoose.connection.on("connected", (err, res) => {
+
+    console.log("mongoose is connected")
+
 })
 
 // server
